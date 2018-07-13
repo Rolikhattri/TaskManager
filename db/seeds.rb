3a3b40684876ab10921a8bfeeeec0797a469c8b6
rolitandon <roli_tandon@bitbucket.org>
2018-07-13 20:32:36 +0530

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.create!({:team_name => "fake team", :start_time => "09:30 PM", :end_time => "10:30 PM"})
+User.create!({:email => "rl@gmail.com", :password => "111111", :password_confirmation => "111111",:admin =>true,:name => "fakeguy", :team_id => 1})
